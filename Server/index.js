7cea379c315c7ee2028ecf25bc16fe7583558853
Manisha52001 <manishahatalageri@gmail.com>
2024-02-20 11:14:59 +0530

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const userRouter = require('./Routes/auth')
+const cookieParser = require('cookie-parser')
+const recipeRouter = require('./Routes/reciper')
+
+const app = express()
+
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT"],
+  credentials: true
+}))
+app.use(cookieParser())
+app.use(express.json())
+app.use('/auth', userRouter)
+app.use('/recipe', recipeRouter)
+
+mongoose.connect('mongodb://127.0.0.1:27017/recipeapp')
+
+app.listen(3001, () => {
+  console.log("Server Started")
+})
